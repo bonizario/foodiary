6bbc89070d77c89d1c0c7ee4c81bcebc629205ad
Gabriel Bonizário <56419828+bonizario@users.noreply.github.com>
2025-07-03 14:21:46 -0300

    feat: persist account in dynamo when signing up user

--- foodiary-api/src/infrastructure/database/dynamo/repositories/account-repository.ts
+++ foodiary-api/src/infrastructure/database/dynamo/repositories/account-repository.ts
@@ -3,21 +3,18 @@ import { Injectable } from "@/core/decorators/injectable";
 import { dynamoClient } from "@/infrastructure/clients/dynamo-client";
 import { AppConfig } from "@/shared/config/app-config";
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { AccountItem } from "../items/account-item";
 
 @Injectable()
 export class AccountRepository {
   constructor(private readonly config: AppConfig) {}
 
   public async create(account: Account): Promise<void> {
+    const accountItem = AccountItem.fromEntity(account);
+
     const command = new PutCommand({
       TableName: this.config.db.dynamo.mainTable,
-      Item: {
-        id: account.id,
-        email: account.email,
-        externalId: account.externalId,
-        type: "Account",
-        PK: `ACCOUNT#${account.id}`,
-      },
+      Item: accountItem.toItem(),
     });
 
     await dynamoClient.send(command);
