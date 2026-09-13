e967841b0ee9030e83885c0ad157ee3f3f5ea699
Gabriel Bonizário <56419828+bonizario@users.noreply.github.com>
2026-09-13 18:32:08 -0300

    chore: include type imports in eslint import order

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ module.exports = defineConfig([
           alphabetize: {
             caseInsensitive: true,
             order: "asc",
+            orderImportKind: "asc",
           },
           groups: [
             "builtin",
